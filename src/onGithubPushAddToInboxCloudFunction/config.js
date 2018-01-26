aa77ad2e5f10fb570fc2b69c7836d8b7e4924107
Steve Conover <sconover@gmail.com>
2018-01-26 18:37:13 +0100

    ci basics
    - cloud function handles github push webhook
    - worker script polls the 'ci inbox' bucket folder and runs an arbitrary command
    - worker script moves the sha file through various folders (in-progress, success or failure) in the bucket
    - cloud function is invoked on file events, and updates the github commit status according to what folder
      the file is moved into

--- src/onGithubPushAddToInboxCloudFunction/config.js
+++ src/onGithubPushAddToInboxCloudFunction/config.js
@@ -8,16 +8,23 @@ const path = require('path');
 nconf
   .argv()
   .env([
-    'CI_BUCKET'
+    'BUCKET',
+    'GCP_PROJECT',
+    'GITHUB_ACCESS_TOKEN',
+    'CI_INBOX_FOLDER'
   ])
   // 3. Config file
   .file({ file: path.join(__dirname, 'config.json') })
   // 4. Defaults
-  .defaults({
-    CI_BUCKET: '',
-  });
+  .defaults({});
 
-checkConfig('CI_BUCKET');
+checkConfig('BUCKET');
+checkConfig('GCP_PROJECT');
+checkConfig('GITHUB_ACCESS_TOKEN');
+checkConfig('CI_INBOX_FOLDER');
+checkConfig('CI_IN_PROGRESS_FOLDER');
+checkConfig('CI_SUCCESS_FOLDER');
+checkConfig('CI_FAILURE_FOLDER');
 
 function checkConfig (setting) {
   if (!nconf.get(setting)) {
