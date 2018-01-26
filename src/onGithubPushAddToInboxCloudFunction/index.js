22daffc0cb8d5359bfa9e409038307839f586def
Steve Conover <sconover@gmail.com>
2018-01-26 07:10:16 +0000

    cloud function improvements

--- src/onGithubPushAddToInboxCloudFunction/index.js
+++ src/onGithubPushAddToInboxCloudFunction/index.js
@@ -1,25 +1,29 @@
 const Storage = require('@google-cloud/storage')
 
-const Storage = require('@google-cloud/storage');
-const config = require('./config');
+const config = require('./config')
 
-const CI_BUCKET = config.get('CI_BUCKET');
+const CI_BUCKET = config.get('CI_BUCKET')
 
 const storage = Storage({
   projectId: config.get('GCP_PROJECT')
 })
-const bucket = storage.bucket(CLOUD_BUCKET);
+const bucket = storage.bucket(CI_BUCKET)
+
+function createInboxGitShaFile(gitSha) {
+  bucket.file("inbox/" + gitSha).save("")
+}
+
 
 /**
  * see https://cloud.google.com/functions/docs/calling/http
  */
-exports.helloHttp = function helloHttp (httpRequest, httpResponse) {
+exports.onGithubPushAddToInboxCloudFunction = function onGithubPushAddToInboxCloudFunction(httpRequest, httpResponse) {
   var gitRefRegex = /.*/
 
   var gitSha = httpRequest.body.after
 
   // may be prefixed with "refs/heads/" or "refs/tags/"
   var gitRef = httpRequest.body.ref
-
+  createInboxGitShaFile(gitSha)
   httpResponse.send(`ciBucket=${CI_BUCKET} gitSha=${gitSha} gitRef=${gitRef} match=${gitRef.match(gitRefRegex)}`)
 }
